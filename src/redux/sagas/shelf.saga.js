13baf5cc3627c4bef2484f9958c4aaf8b182bcb6
Heather Kim <heatherkim0405@gmail.com>
2022-03-09 15:38:39 -0600

    trying to create a reducer to shelf items

---
+++ src/redux/sagas/shelf.saga.js
@@ -0,0 +1,28 @@
+import axios from 'axios';
+import { put, takeLatest } from 'redux-saga/effects';
+
+// Fetch items 
+function* fetchItems() {
+  try {
+    // const config = {
+    //   headers: { 'Content-Type': 'application/json' },
+    //   withCredentials: true,
+    // };
+
+    // // the config includes credentials which
+    // // allow the server session to recognize the user
+    // // If a user is logged in, this will return their information
+    // // from the server session (req.user)
+    const response = yield axios.get('/api/shelf');
+    console.log('response.data is', response.data);
+    yield put({ type: 'SET_ITEMS', payload: response.data });
+  } catch (error) {
+    console.log('User get request failed', error);
+  }
+}
+
+function* shelfSaga() {
+  yield takeLatest('FETCH_ITEMS', fetchItems);
+}
+
+export default shelfSaga;
