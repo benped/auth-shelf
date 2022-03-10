626ae01e0eb8ddefb3ef9f1bbd800e1f222ae01d
Leah Atkins <leah.atkins84@gmail.com>
2022-03-10 12:08:36 -0600

    Added info page

--- src/components/InfoPage/InfoPage.jsx
+++ src/components/InfoPage/InfoPage.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import {useDispatch, useSelector} from 'react-redux';
 
 // This is one of our simplest components
 // It doesn't have local state
@@ -6,9 +7,22 @@ import React from 'react';
 // or even care what the redux state is
 
 function InfoPage() {
+  const books = useSelector(store => store.shelf);
+  const dispatch = useDispatch();
+ 
+  useEffect(() => {
+    dispatch({type:'FETCH_BOOKS'})
+  }, [])
+console.log(books);
   return (
     <div className="container">
-      <p>Info Page</p>
+      <p>Info Page
+      </p>
+      <ul>
+      {books.map((book, i) =>{
+       return <li key={i}>{book.description}</li>
+      })}
+      </ul>
     </div>
   );
 }
